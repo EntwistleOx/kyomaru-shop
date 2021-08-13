3682b0a90ff35f447b94671bb54ad989141d5515
Juan Díaz Ortega <juan.diaz.12069@gmail.com>
2021-08-13 15:15:10 -0400

    add contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,27 +1,31 @@
 <template>
-  <div>Contacto</div>
+  <v-container class="mt-5">
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-parallax src="contact.jpg"> </v-parallax>
+      </v-col>
+      <v-col cols="12" md="6"> </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
-  mounted() {
-    this.sendMail();
-  },
   methods: {
-    async sendMail() {
-      const mail = {
-        to: "nodemailerADL@gmail.com",
-        subject: "Hola desde JD",
-        params: { text: "briiiigido!" },
-      };
-      await fetch("http://localhost:3000/mail", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(mail), // body data type must match "Content-Type" header
-      });
-    },
+    // async sendMail() {
+    //   const mail = {
+    //     to: 'nodemailerADL@gmail.com',
+    //     subject: 'Hola desde JD',
+    //     params: { text: 'briiiigido!' },
+    //   };
+    //   await fetch('http://localhost:3000/mail', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify(mail), // body data type must match "Content-Type" header
+    //   });
+    // },
   },
 };
 </script>
